5a09fc58c403aec53c04facd04b0d61f7deb58c1
Atul Singh <atulsingh74088@gmail.com>
2026-07-23 22:54:26 +0530

    Added code

--- stringplaylist/StrPractice.java
+++ stringplaylist/StrPractice.java
@@ -14,6 +14,11 @@ public class StrPractice {
 
        System.out.println(longestPalindrome(str1));
 
+       String str = "1234";
+       String[] str12 = str.split("");
+        System.out.println(str12[0]);
+
+
 
 //        System.out.println(checkAnagram("litsen","silent"));
 
@@ -578,5 +583,23 @@ public class StrPractice {
             result ^= c;
         }
         return result;
+    }
+
+    public static String addStrings(String num1, String num2) {
+            int i = num1.length()-1;
+            int j = num2.length()-1;
+            int carry = 0;
+            StringBuilder stringBuilder = new StringBuilder();
+            while (i>=0 || j>=0 || carry != 0){
+                int x = i>=0? num1.charAt(i)-'0':0;
+                int y = j>=0? num2.charAt(j)-'0':0;
+                int sum = x + y + carry;
+                stringBuilder.append(sum%10);
+                carry=  sum/10;
+                 i--;
+                 j--;
+
+            }
+            return stringBuilder.reverse().toString();
     }
 }
